1f0dd76ff824c88028228c5609dabc396e8d75bb
MrMJY <m1635836340@163.com>
2019-06-28 16:55:08 +0800

    评论管理及细节完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -5,7 +5,8 @@
          slot="header">
       评论管理
     </div>
-    <el-table :data="commentList">
+    <el-table :data="commentList"
+              v-loading="load_comments">
       <el-table-column prop="title"
                        align="center"
                        label="标题"></el-table-column>
@@ -18,7 +19,9 @@
       <el-table-column label="操作"
                        align="center">
         <template slot-scope="scope">
-          <el-switch v-model="scope.row.comment_status"
+          <el-switch :disabled="scope.row.chang_status"
+                     v-model="scope.row.comment_status"
+                     @change="handleChangeStatus(scope.row)"
                      active-color="#13ce66"
                      inactive-color="#ff4949">
           </el-switch>
@@ -27,6 +30,7 @@
     </el-table>
     <el-pagination background
                    class="page-list"
+                   :disabled="load_comments"
                    @current-change="handleCurrentChange"
                    layout="prev, pager, next"
                    :total="total_count">
@@ -42,7 +46,8 @@ export default {
     return {
       commentList: [],
       total_count: 1,
-      current_page: 1
+      current_page: 1,
+      load_comments: false
     };
   },
   created () {
@@ -50,6 +55,7 @@ export default {
   },
   methods: {
     loadCommentLists (page = 1) {
+      this.load_comments = true;
       this.$http({
         method: 'GET',
         url: '/articles',
@@ -59,14 +65,47 @@ export default {
         }
       }).then(res => {
         console.log(res);
+        res.results.forEach(item => {
+          item.chang_status = false;
+        });
         this.total_count = res.total_count;
         this.commentList = res.results;
+        this.load_comments = false;
+      }).catch(err => {
+        console.log(err);
+        this.$message.error('获取失败');
       });
     },
 
     handleCurrentChange (page) {
       this.current_page = page;
       this.loadCommentLists(page);
+    },
+
+    handleChangeStatus (item) {
+      item.chang_status = true;
+      console.log(item);
+      this.$http({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: item.id + ''
+        },
+        data: {
+          allow_comment: item.comment_status
+        }
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: item.comment_status ? '开启评论' : '关闭评论'
+        });
+        item.chang_status = false;
+      }).catch(err => {
+        console.log(err);
+        item.comment_status = !item.comment_status;
+        item.$message.error('操作失败');
+        item.chang_status = false;
+      });
     }
   }
 };
